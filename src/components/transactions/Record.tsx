34975285fd99141332aa7a14debdb2e101827fb7
Alvin Tayag <tmalvin.workshop@gmailcom>
2019-12-18 23:31:11 +0800

    create codesandbox

--- src/components/transactions/Record.tsx
+++ src/components/transactions/Record.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { IonInput, IonItem, IonLabel, IonContent } from '@ionic/react';
+import { IonSelect, IonSelectOption, IonInput, IonItem, IonLabel, IonContent } from '@ionic/react';
 
 const Record: React.FC = () => (
     <IonContent>
       <IonItem>
-        <IonLabel position="stacked">Description</IonLabel>
-        <IonInput></IonInput>
+        <IonLabel position="stacked">Category</IonLabel>
+        <IonSelect placeholder="Category" interface="popover">
+            <IonSelectOption value="Rent">Rent</IonSelectOption>
+            <IonSelectOption value="Grocery">Grocery</IonSelectOption>
+            <IonSelectOption value="Baby Funds">Baby Funds</IonSelectOption>
+            <IonSelectOption value="Insurance">Insurance</IonSelectOption>
+            <IonSelectOption value="Utilities">Utilities</IonSelectOption>
+            <IonSelectOption value="Allowance">Allowance</IonSelectOption>
+            <IonSelectOption value="Rolling Funds">Rolling Funds</IonSelectOption>
+            <IonSelectOption value="Mobile">Mobile</IonSelectOption>
+          </IonSelect>
       </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Description</IonLabel>
+          <IonInput required></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Amount</IonLabel>
+          <IonInput type="number" placeholder="0.0" required></IonInput>
+        </IonItem>
     </IonContent>
   );
 
